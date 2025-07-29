eb0fd35c81f6e2ee5279cdcc8562a867ea15f201
Oleksandr Radionov <fokk190e@gmail.com>
2025-07-29 20:39:13 +0300

    Change App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -28,6 +25,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h2>Hello world!</h2>
     </>
   )
 }
